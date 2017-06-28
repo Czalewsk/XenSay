052ebf2ebffe1df9a5b346363ab117c39dfda7e9
Garrus2142 <timothee.hugo@gmail.com>
2017-06-28 19:32:14 +0200

    Add select music in learn mode

--- PoC/run_learn.c
+++ PoC/run_learn.c
@@ -3,74 +3,74 @@
 #include "events.h"
 #include "button.h"
 
-static void press_learn(u32 button)
+static u8 state;
+
+static void onPressButtonError(u32 button)
 {
-    if (button & BTN_CFG_2) // Prev
+    event_setState(CONFIG);
+}
+
+static void error()
+{
+    lcd_write_line(" Press to quit  ", 1);
+    setOnPressCallback(&onPressButtonError);
+}
+
+static void onPressButtonGame(u32 button)
+{
+    
+}
+
+static void onPressButtonSelect(u32 button)
+{
+    u8 *name;
+    
+    if (button & BTN_CFG_1) // Ok
     {
-        u8 *name;
-        if ((name = xformat_prevMusic()))
-        {
-            name[17] = '\0';
-            lcd_write_line("                ", 0);
-            lcd_write_line(name, 0);   
-        }
-        //xformat_nextMusic(&ptr);
+        //setOnPressCallback(NULL);
+        music_play(xformat_loadMusic(), xformat_sizeMusic());
+    }
+    else if (button & BTN_CFG_2) // Prev
+    {
+        if (!(name = xformat_prevMusic()))
+            error();
+        lcd_shift("", 1);
+        lcd_shift(name, 1);
     }
     else if (button & BTN_CFG_3) // Next
     {
-                 
-        //lcd_write_line("      D          ", 0);
-        //lcd_write_line(xformat_nextMusic(&ptr), 0);
-        u8 *name;
-        if ((name = xformat_nextMusic()))
-        {
-            name[17] = '\0';
-            lcd_write_line("                ", 0);
-            lcd_write_line(name, 0);   
-        }
+        if (!(name = xformat_nextMusic()))
+            error();
+        lcd_shift("", 1);
+        lcd_shift(name, 1);
     }
-    else if (button & BTN_CFG_1) // OK
+}
+
+static void SelectMusic()
+{   
+    u8 *name;
+    
+    lcd_write_line("\177 Select Music \176", 0);
+    
+    if (!(name = xformat_firstMusic()))
     {
-        u8 *musicData;
-        u16 musicSize;
-        
-        if (!(musicData = xformat_loadMusic()))
-            return ;
-        if (!(musicSize = xformat_sizeMusic()))
-            return ;
-        music_play(musicData, musicSize);
+        lcd_write_line("No music in card", 0);
+        error();
+        return ;
     }
+    
+    lcd_shift(name, 1);
+    
+    setOnPressCallback(&onPressButtonSelect);
 }
 
 void    run_learn(void)
 {
-    //lcd_write_line("\177 Select Music \176", 0);
-//    lcd_write_line("\177              \176", 1);
-    /*lcd_write_line("Checking sdcard ", 0);
-    lcd_write_line("                ", 1);
-   
-    if (sdcard_start())
+    if (!xformat_start())
     {
-        if (sdcard_read(0))
-           lcd_write_line("Done            ", 0);
+        error();
+        return ;
     }
-      setOnPressCallback(&press_learn);
-    event_setState(CONFIG);*/
     
-    if (xformat_start())
-    {
-        u8 *name;
-        if ((name = xformat_firstMusic()))
-        {
-            name[17] = '\0';
-            lcd_write_line("                ", 0);
-            lcd_write_line(name, 0);
-        }
-        else
-            lcd_write_line("ERROR first     ", 0);
-    }
-    else
-        lcd_write_line("ERROR START     ", 0);
-    setOnPressCallback(&press_learn);
-    setOnReleaseCallback(NULL);
+    SelectMusic();
 }
